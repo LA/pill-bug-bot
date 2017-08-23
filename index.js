beae73a3c59fdc07f66bbcdbbae4b6cb4c8f3643
Adar <theadarbutel@gmail.com>
2017-08-22 18:23:23 -0700

    Add express for heroku

--- index.js
+++ index.js
@@ -3,36 +3,51 @@ require('dotenv').config();
 const Snoowrap = require('snoowrap');
 const Snoostorm = require('snoostorm');
 
-// Build snoowrap and snoostorm clients
-const r = new Snoowrap({
-  userAgent: process.env.USER_AGENT,
-  clientId: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
-  username: process.env.REDDIT_USER,
-  password: process.env.REDDIT_PASS
-});
-const client = new Snoostorm(r);
+const express = require('express');
+const app = express();
 
-const streamOptions = {
-  subreddit: 'all',
-  results: 25
-};
+const port = process.env.PORT || 3000;
 
-// Create Snoostorm CommentStream with the specified options
-const comments = client.CommentStream(streamOptions);
+const log = (text) => {
+  console.log('[' + new Date().toUTCString() + ']', text);
+}
 
-// On comment, roll
-comments.on('comment', (comment) => {
-  let body = comment.body;
-  body = body.split(' ');
-  if (body[0] == '!PBRoll' && body.length > 1) {
-    let num = body[1];
-    num = parseInt(num);
-    if (typeof num == 'number' && num > 0) {
-      const startNum = 1;
-      const returnNum = Math.floor(Math.random() * num) + startNum;
-      comment.reply('Rollie Pollie rolled ' + returnNum + '.');
-      console.log('Rollie Pollie rolled ' + returnNum + '.');
+app.listen(port, () => {
+  console.log('Listening on Port:', port);
+
+  // Build snoowrap and snoostorm clients
+  const r = new Snoowrap({
+    userAgent: process.env.USER_AGENT,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    username: process.env.REDDIT_USER,
+    password: process.env.REDDIT_PASS
+  });
+  const client = new Snoostorm(r);
+
+  const streamOptions = {
+    subreddit: 'all',
+    results: 100,
+    pollTime: 2000,
+  };
+
+  // Create Snoostorm CommentStream with the specified options
+  const comments = client.CommentStream(streamOptions);
+
+  // On comment, roll
+  comments.on('comment', (comment) => {
+    let body = comment.body;
+    body = body.split(' ');
+    if (body[0] == '!PBRoll' && body.length > 1) {
+      log('PBRoll Found!');
+      let num = body[1];
+      num = parseInt(num);
+      if (typeof num == 'number' && num > 0) {
+        const startNum = 1;
+        const returnNum = Math.floor(Math.random() * num) + startNum;
+        comment.reply('Rollie Pollie rolled ' + returnNum + '.');
+        log('Rollie Pollie rolled ' + returnNum + '.');
+      }
     }
-  }
+  });
 });
